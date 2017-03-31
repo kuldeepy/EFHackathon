150cd8e9929422be3967755bd2acf2ca60a321ff
kuldeep yadav <kuldeep6.in@gmail.com>
2017-03-31 17:47:16 +0530

    Created project direactory structure

---
+++ Scripts/App/app.js
@@ -0,0 +1,14 @@
+﻿//Define an angular module for our app
+var hackDayApp = angular.module('hackDayApp', ['ngAnimate', 'ngRoute', 'ngSanitize', 'ui.bootstrap']);
+//Define Routing for the application
+hackDayApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'Views/Home.html',
+                controller: 'HomeController'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }]);
